d0529f7d4a9005e1eb17633a8294c6d19a55c470
Hannah Bradshaw <79159808+hannah-bradshaw@users.noreply.github.com>
2021-04-06 20:44:46 +0100

    final code

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function send() {
     let firstName = document.getElementById("fname").value;
     let lastName = document.getElementById("lname").value;
 
-    alert(`Thank you, ${firstName} ${lastName} for submitting your response! We will get back to you as soon as possible`);
+    alert(`Thank you, ${firstName} for submitting your response. We will get back to you as soon as possible!`);
 }
